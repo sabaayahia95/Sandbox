08fa7a4082db5d8c73364f4aed0dd6706b5fe516
sabaa yahia <sabaa yahia4@gmail.com>
2020-09-05 17:37:55 +0300

    Initial commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var experiencePoints = 5 ;
+    val playerName ="Estragon";
+
+   experiencePoints= experiencePoints+5 ;
+    println(experiencePoints );
+
+    println(playerName) ;
+
+    var magic=  playerName.reversed()
+    println(magic)
+    var hasSteed = false
+    if (hasSteed==false){
+        println("they dont have dragon")
+    }
+    else {
+        println("they have dragon")
+    }
+    fun playerState(){
+        var hasSteed =0;
+    }
+
+
+
+
+}
